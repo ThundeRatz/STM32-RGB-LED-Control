6298225971dfedc393ed3b4b0cdd8d0720728439
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-09-16 01:11:07 -0300

    :white_check_mark: Add button test

---
+++ tests/bin/test_button.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file test_buttons.cpp
+ *
+ * @brief Test Buttons
+ *
+ * @date 09/2024
+ */
+
+#include "test_core.hpp"
+#include "button.hpp"
+
+int main() {
+    test_core_init();
+    Button button(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+
+    while (true) {
+        if (button.is_pressed()) {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+        } else {
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+        }
+    }
+}
