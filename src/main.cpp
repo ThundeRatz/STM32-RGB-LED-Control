4693b6bd84cb39744ca8989ddbfa39e49bd70bbd
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-09-16 01:10:25 -0300

    :sparkles: Implement main

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@
  * @brief Main function
  */
 
+#include <adc.h>
+#include <stm32f1xx_hal_gpio.h>
+#include <tim.h>
 #include "mcu.hpp"
+#include "potentiometer.hpp"
+#include "rgb.hpp"
+#include "button.hpp"
 
 /*****************************************
  * Private Constant Definitions
@@ -19,7 +25,16 @@ static constexpr uint16_t led_toggle_delay_ms = 1500;
 int main(void) {
     hal::mcu::init();
 
+    Button        button(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
+    Potentiometer potentiometer(&hadc1, MX_ADC1_Init);
+    RGB           rgb(&htim1, MX_TIM1_Init, TIM_CHANNEL_1, TIM_CHANNEL_2, TIM_CHANNEL_3);
+
     for (;;) {
-        hal::mcu::sleep(led_toggle_delay_ms);
+        if (button.is_pressed()) {
+            rgb.set_color(255, 255, 255);
+        } else {
+            uint16_t potentiometer_value = potentiometer.get_value();
+            rgb.set_color(potentiometer_value >> 8, potentiometer_value >> 8, potentiometer_value >> 8);
+        }
     }
 }
